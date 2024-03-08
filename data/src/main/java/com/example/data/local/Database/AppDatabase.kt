e50a5c51d0d010d0e266b17484a023a141ecab7c
murad <mouradhany574@gmail.com>
2024-03-08 20:17:47 +0200

    edit price and rate text size

--- data/src/main/java/com/example/data/local/Database/AppDatabase.kt
+++ data/src/main/java/com/example/data/local/Database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.data.local.Dao.ProductDao
 import com.example.domain.entities.Product
 
-@Database(entities = [Product::class], version = 1)
+@Database(entities = [Product::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun productDao():ProductDao
 }
